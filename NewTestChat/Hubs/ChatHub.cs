d8a7544826a17d985de7a25e0b94ced9693383fd
Rakka Rage <rakkarage@gmail.com>
2019-01-21 19:43:37 -0500

    x

--- NewTestChat/Hubs/ChatHub.cs
+++ NewTestChat/Hubs/ChatHub.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
-using ca.HenrySoftware;
-using System.Diagnostics;
 namespace NewTestChat
 {
 	public class ChatHub : Hub
 	{
-		private Gename _gename = new Gename();
 		public async Task SendMessage(string user, string message)
 		{
-			var name = _gename.Name();
-			Debug.Print(name);
 			await Clients.All.SendAsync("ReceiveMessage", user, message);
 		}
 	}
