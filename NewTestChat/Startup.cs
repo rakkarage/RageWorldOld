39a6c534478f3ea53a73f6d095b948cc8e2efc9e
Rakka Rage <rakkarage@gmail.com>
2019-01-23 09:25:59 -0500

    x

--- NewTestChat/Startup.cs
+++ NewTestChat/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,10 @@ namespace NewTestChat
 				app.UseExceptionHandler("/Error");
 				app.UseHsts();
 			}
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			});
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 			app.UseSignalR(routes =>
